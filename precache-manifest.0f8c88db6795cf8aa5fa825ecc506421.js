66da591d21dd7e23ee79e73a4c21b4079479bbfc
kshunn <qkshunn@gmail.com>
2020-03-03 17:35:04 +0900

    Updates

---
+++ precache-manifest.0f8c88db6795cf8aa5fa825ecc506421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3753f9b575940a964faeae8b7dcc4b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b1964a6dc2f4fa3613d6",
+    "url": "/movie_app/static/css/main.1d192314.chunk.css"
+  },
+  {
+    "revision": "79b3d98d9236aa532655",
+    "url": "/movie_app/static/js/2.3aa1e297.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.3aa1e297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1964a6dc2f4fa3613d6",
+    "url": "/movie_app/static/js/main.a8959858.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
