d8388da44e45fffb368233569dcab7c667136d8b
kshunn <qkshunn@gmail.com>
2020-03-03 17:28:16 +0900

    CSS applied

--- src/Movie.js
+++ src/Movie.js
@@ -9,15 +9,17 @@ function Movie({year, title, summary, poster, genres}){
             <div className="movie_data">
                 <h3 className="movie_title">{title}</h3>
                 <h5 className="movie_year">{year}</h5>
-                <p className="movie_summary">{summary}</p>
-                <ul className="movie_genres">{genres.map(genre => <li className="genres_genre">{genre}</li>)}</ul>
+                <ul className="movie_genres">
+                    {genres.map((genre, index) => (<li key={index} className="genres_genre">{genre}</li>))}
+                </ul>
+                <p className="movie_summary">{summary.slice(0, 140)}...</p>
             </div>
         </div>
     );
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired, 
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
